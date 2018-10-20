0d2c4faa9e73455fdc34506b30f60a225ec8a879
kojimain <kojimainjp@gmail.com>
2018-10-21 01:02:16 +0900

    set default_locale to ja

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module SimplebbsApp
   class Application < Rails::Application
+    # set default_locale to :ja
+    I18n.config.available_locales = :ja
+    I18n.default_locale = :ja
+    config.i18n.default_locale = :ja
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
