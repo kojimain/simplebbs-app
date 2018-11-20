f363b40e0167ba03ecbf60c93c225b0353d9f419
kojimain <kojimainjp@gmail.com>
2018-11-20 21:27:13 +0900

    RSpec Api::BbsPostsController#create

--- spec/controllers/api/bbs_posts_controller_spec.rb
+++ spec/controllers/api/bbs_posts_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Api::BbsPostsController, type: :controller do
 
+  describe "POST #create" do
+    let(:bbs_post_create_form_params) {
+      {
+        body: "test bbs_post body\nhoge\nfuga",
+        poster_name: "test poster_name",
+      }
+    }
+    before do
+      @bbs_thread = FactoryBot.create(:bbs_thread)
+    end
+    
+    it "returns http success" do
+      post :create, params: {bbs_thread_id: @bbs_thread.id, bbs_posts_create_form: bbs_post_create_form_params}
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "adds a bbs_post" do
+      expect {
+        post :create, params: {bbs_thread_id: @bbs_thread.id, bbs_posts_create_form: bbs_post_create_form_params}
+      }.to change(@bbs_thread.bbs_posts, :count).by(1)
+    end
+  end
+
 end
