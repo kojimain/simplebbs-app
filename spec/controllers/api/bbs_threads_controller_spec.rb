ec9e4631f499d600963c8659681ccf4e7784ee3d
kojimain <kojimainjp@gmail.com>
2018-11-20 21:14:59 +0900

    RSpec Api::BbsThreadsController#create

--- spec/controllers/api/bbs_threads_controller_spec.rb
+++ spec/controllers/api/bbs_threads_controller_spec.rb
@@ -3,15 +3,21 @@ require 'rails_helper'
 RSpec.describe Api::BbsThreadsController, type: :controller do
 
   describe "POST #create" do
-    let(:bbs_thread_form_params) {
+    let(:bbs_thread_create_form_params) {
       {
         title: "test bbs_thread title",
       }
     }
     
     it "returns http success" do
-      post :create, params: {bbs_threads_create_form: bbs_thread_form_params}
+      post :create, params: {bbs_threads_create_form: bbs_thread_create_form_params}
       expect(response).to have_http_status(:success)
+    end
+    
+    it "adds a bbs_post" do
+      expect {
+        post :create, params: {bbs_threads_create_form: bbs_thread_create_form_params}
+      }.to change(BbsThread.all, :count).by(1)
     end
   end
 
