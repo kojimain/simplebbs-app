a2c3d6718cfa00881e225108910a9c38dd9f476d
kojimain <kojimainjp@gmail.com>
2018-11-20 21:07:22 +0900

    RSpec BbsThreadsController#show

--- spec/controllers/bbs_threads_controller_spec.rb
+++ spec/controllers/bbs_threads_controller_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe BbsThreadsController, type: :controller do
 
   describe "GET #show" do
+    before do
+      @bbs_thread = FactoryBot.create(:bbs_thread)
+    end
+    
     it "returns http success" do
-      get :show
+      get :show, params: { id: @bbs_thread.id }
       expect(response).to have_http_status(:success)
     end
   end
